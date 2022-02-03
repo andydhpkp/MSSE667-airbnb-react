7f91b797677b6d426be9c9dce9a2f027629f21c5
Andrew Durham <andrewdurham1094@gmail.com>
2022-02-03 10:05:36 -0700

    finished assignment 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+/* import React, {Component} from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, World Implementation!</h1>
+      </div>
+    )
+  }
+}
+
+export default App */
+
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [listings, setListings] = useState(null);
+  const apiURL = "http://localhost:8082/listings/";
+  const fetchData = async () => {
+    const response = await axios.get(apiURL);
+    setListings(response.data)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Display Listings</h1>
+
+      <div>
+        <button className="fetch-btn" onClick={fetchData}>
+          Display
+        </button>
+        <br />
+      </div>
+      <div className="listings">
+        {listings && listings.map((listing, index) => {
+          if(listing.name === null) {
+
+          } else {
+          return (
+            <div className="listing" key={index}>
+              <h2>Listing {index}</h2>
+              <h3>{listing.name}</h3>
+            </div>
+          )
+          }
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
